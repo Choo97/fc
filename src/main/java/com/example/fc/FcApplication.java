ae67b544cd87c4b73adbb4d63c532e01c4cce5f7
bananaroti <gh3298@gmail.com>
2023-09-07 18:13:49 +0900

    이지윅에디터 서버경로수정, 리소스핸들러 서버경로 수정

--- src/main/java/com/example/fc/FcApplication.java
+++ src/main/java/com/example/fc/FcApplication.java
@@ -4,10 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Component;
-import org.springframework.web.servlet.DispatcherServlet;
 
 @SpringBootApplication
 public class FcApplication extends SpringBootServletInitializer {
@@ -18,9 +16,7 @@ public class FcApplication extends SpringBootServletInitializer {
     }
 
     public static void main(String[] args) {
-        ApplicationContext applicationContext = SpringApplication.run(FcApplication.class, args);
-        DispatcherServlet dispatcherServlet = (DispatcherServlet)applicationContext.getBean("dispatcherServlet");
-        dispatcherServlet.setThrowExceptionIfNoHandlerFound(true);
+        SpringApplication.run(FcApplication.class, args);
     }
 
 }
