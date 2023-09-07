c05bfdcfdf31e6c2ba3f105d22bcfb43455a7063
bananaroti <gh3298@gmail.com>
2023-09-07 16:14:47 +0900

    이지윅에디터 서버경로수정, 리소스핸들러 서버경로 수정

--- src/main/java/com/example/fc/EpRecruitMvcConfig.java
+++ src/main/java/com/example/fc/EpRecruitMvcConfig.java
@@ -5,10 +5,13 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.multipart.commons.CommonsMultipartResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.resource.PathResourceResolver;
 
 @Configuration
+@EnableWebMvc
 public class EpRecruitMvcConfig implements WebMvcConfigurer {
     @Value("${getEpRecruitPath}")
     String getEpRecruitPath;
@@ -27,7 +30,7 @@ public class EpRecruitMvcConfig implements WebMvcConfigurer {
 //    프로젝트경로 외부에서 디렉토리 접근하는법
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler(uploadEpRecruit).addResourceLocations(getEpRecruitMainThumbnailPath);
-        registry.addResourceHandler(uploadEpRecruitContent).addResourceLocations(getEpRecruitContentPath); //에디터사진업로드경로
+        registry.addResourceHandler(uploadEpRecruitContent).addResourceLocations(getEpRecruitContentPath).setCachePeriod(3600).resourceChain(true).addResolver(new PathResourceResolver()); //에디터사진업로드경로
     }
 
 //    @Bean
